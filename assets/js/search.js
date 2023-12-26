c3f8ebd408eb8d8369e1c4d5a0887348a9908afd
Iefan <66840141+realiefan@users.noreply.github.com>
2023-12-27 03:49:23 +0530

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,7 +6,7 @@
    // Add your logic to perform the Google search with the searchTerm
    // For example, you can redirect the user to the Google search results page
    window.location.href =
-     "https://duckduckgo.com/?t=h_&q=" + encodeURIComponent(searchTerm);
+     "https://duckduckgo.com/?q=" + encodeURIComponent(searchTerm) + "&hps=1&start=1&ia=web";
  }
 
 document.getElementById("googleSearch").addEventListener("keyup", function(event) {
