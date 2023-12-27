a03118ce8daf36eeccd6671fae7ee0922c3e8a6a
Iefan <66840141+realiefan@users.noreply.github.com>
2023-12-27 20:28:01 +0000

    search update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,16 +1,35 @@
+function performSearch() {
+  var searchEngine = document.getElementById("searchEngineDropdown").value;
+  var searchTerm = document.getElementById("googleSearch").value;
 
+  // Add your logic to perform the search based on the selected search engine and searchTerm
+  if (searchEngine === "google") {
+    window.location.href =
+      "https://www.google.com/search?q=" + encodeURIComponent(searchTerm);
+  } else if (searchEngine === "duckduckgo") {
+    window.location.href =
+      "https://duckduckgo.com/?q=" + encodeURIComponent(searchTerm);
+  } else if (searchEngine === "reddit") {
+    window.location.href =
+      "https://www.reddit.com/search?q=" + encodeURIComponent(searchTerm);
+  } else if (searchEngine === "twitter") {
+    window.location.href =
+      "https://twitter.com/search?q=" + encodeURIComponent(searchTerm);
+  } else if (searchEngine === "youtube") {
+    window.location.href =
+      "https://www.youtube.com/results?search_query=" +
+      encodeURIComponent(searchTerm);
+  } else if (searchEngine === "nostr") {
+    window.location.href =
+      "https://nostr.band/?q=" + encodeURIComponent(searchTerm);
+  }
+  // Add more conditions for other search engines
+}
 
-
- function performGoogleSearch() {
-   var searchTerm = document.getElementById("googleSearch").value;
-   // Add your logic to perform the Google search with the searchTerm
-   // For example, you can redirect the user to the Google search results page
-   window.location.href =
-     "https://duckduckgo.com/?q=" + encodeURIComponent(searchTerm) + "&hps=1&start=1&ia=web";
- }
-
-document.getElementById("googleSearch").addEventListener("keyup", function(event) {
+document
+  .getElementById("googleSearch")
+  .addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-        performGoogleSearch();
+      performSearch();
     }
-});
+  });
