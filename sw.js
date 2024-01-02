6d01d3b5d61079e1c338a0c0c9a9eabd7303cd21
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-03 03:11:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 const CACHE_PREFIX = "NostrNet";
-const CACHE_VERSION = "V1.3"; // Update the cache version when you make changes to the caching logic
+const CACHE_VERSION = "V1.5"; // Update the cache version when you make changes to the caching logic
 const CACHE_NAME_STATIC = `${CACHE_PREFIX}-static-${CACHE_VERSION}`;
 const CACHE_NAME_DYNAMIC = `${CACHE_PREFIX}-dynamic-${CACHE_VERSION}`;
 
@@ -40,15 +38,26 @@ workbox.routing.registerRoute(
   })
 );
 
+// Cache icons using the cache-first strategy
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.startsWith("/icon/"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "icon-cache",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50, // adjust as needed
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+      }),
+    ],
+  })
+);
+
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames
-          .filter(
-            (cache) =>
-              cache.startsWith(CACHE_PREFIX) && !cache.includes(CACHE_VERSION)
-          )
+          .filter((cache) => cache.startsWith(CACHE_PREFIX) && !cache.includes(CACHE_VERSION))
           .map((cache) => caches.delete(cache))
       );
     })
@@ -72,7 +81,7 @@ workbox.routing.registerRoute(
 
 // Enable Navigation Preload
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
+  if (event.request.mode === "navigate" && self.registration.navigationPreload) {
     event.respondWith(
       (async () => {
         const preloadResponse = await event.preloadResponse;
@@ -88,22 +97,21 @@ self.addEventListener("fetch", (event) => {
   }
 });
 
+// Fetch and cache icons
 self.addEventListener("fetch", (event) => {
-  if (
-    event.request.mode === "navigate" &&
-    self.registration.navigationPreload
-  ) {
+  if (event.request.url.startsWith("https://icon.horse/icon/")) {
     event.respondWith(
-      (async () => {
-        const preloadResponse = await event.preloadResponse;
-
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      })()
+      caches.match(event.request).then((cachedResponse) => {
+        return (
+          cachedResponse ||
+          fetch(event.request).then((networkResponse) => {
+            caches.open("icon-cache").then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+            return networkResponse;
+          })
+        );
+      })
     );
   }
 });
