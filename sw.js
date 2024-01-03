1772cd2dd4d431ac7f028a41e277d742070835f1
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-04 01:17:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 const CACHE_PREFIX = "NostrNet";
-const CACHE_VERSION = "V3.4.7";
+const CACHE_VERSION = "V3.4.8";
 const CACHE_NAME_STATIC = `${CACHE_PREFIX}-static-${CACHE_VERSION}`;
 const CACHE_NAME_DYNAMIC = `${CACHE_PREFIX}-dynamic-${CACHE_VERSION}`;
 const ICON_CACHE_NAME = `${CACHE_PREFIX}-icon-${CACHE_VERSION}`;
@@ -80,7 +80,7 @@ workbox.routing.registerRoute(
 
 // Cache icons based on their unique URLs with CacheFirst strategy
 workbox.routing.registerRoute(
-  ({ url }) => url.pathname.startsWith("/icon/"),
+  ({ url }) => url.pathname.startsWith("https://icon.horse/icon/"),
   new workbox.strategies.CacheFirst({
     cacheName: ICON_CACHE_NAME,
     plugins: [
