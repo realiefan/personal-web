6a2c01186839c61829231bfe61bc606bd8b3a499
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-04 12:05:48 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,142 +1,75 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 const CACHE_PREFIX = "NostrNet";
-const CACHE_VERSION = "V3.4.17";
+const CACHE_VERSION = "V5";
 const CACHE_NAME_STATIC = `${CACHE_PREFIX}-static-${CACHE_VERSION}`;
 const CACHE_NAME_DYNAMIC = `${CACHE_PREFIX}-dynamic-${CACHE_VERSION}`;
 const ICON_CACHE_NAME = `${CACHE_PREFIX}-icon-${CACHE_VERSION}`;
 
-// Function to handle cache cleanup on activate
-const cleanupOldCaches = async () => {
-  const cacheNames = await caches.keys();
-  return Promise.all(
-    cacheNames.map((cacheName) => {
-      if (
-        cacheName.startsWith(CACHE_PREFIX) &&
-        ![CACHE_NAME_STATIC, CACHE_NAME_DYNAMIC, ICON_CACHE_NAME].includes(
-          cacheName
-        )
-      ) {
-        return caches.delete(cacheName);
-      }
-    })
-  );
+const cacheSettings = {
+  cacheName: CACHE_NAME_STATIC,
+  plugins: [
+    new workbox.expiration.ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+    }),
+  ],
 };
 
+// Cleanup old caches on activate
 self.addEventListener("activate", (event) => {
-  event.waitUntil(Promise.all([cleanupOldCaches(), scheduleNotifications()]));
+  event.waitUntil(
+    Promise.all([workbox.cacheNames.cleanupSome({ prefix: CACHE_PREFIX }), scheduleNotifications()])
+  );
 });
 
-// Cache static files (HTML, JS, CSS, SVG, PNG) with StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  /\.(html|js|css|svg|png|jpg|jpeg|gif)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE_NAME_STATIC,
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [200],
-        headers: { "Cache-Control": "public, max-age=31536000" }, // Cache for 1 year
-      }),
-    ],
-  })
-);
+// Cache static files with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(/\.(html|js|css|svg|png|jpg|jpeg|gif)$/, new workbox.strategies.StaleWhileRevalidate(cacheSettings));
 
-// Cache images using the StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  /\.(png|jpg|jpeg|gif)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: `${CACHE_PREFIX}-images`,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50, // Adjust as needed
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-      }),
-    ],
-  })
-);
+// Cache images with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(/\.(png|jpg|jpeg|gif)$/, new workbox.strategies.StaleWhileRevalidate(cacheSettings));
 
-// Cache icons based on their unique URLs with StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  ({ url }) => url.pathname.startsWith("https://icon.horse/icon/"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: ICON_CACHE_NAME,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50, // Adjust as needed
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-      }),
-    ],
-  })
-);
+// Cache icons with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(({ url }) => url.pathname.startsWith("https://icon.horse/icon/"), new workbox.strategies.StaleWhileRevalidate(cacheSettings));
 
-// Background sync to update the cache for dynamic content
-const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin(
-  "bgSyncQueue",
-  {
-    maxRetentionTime: 24 * 60, // Retry for up to 24 hours
-  }
-);
+// Cache dynamic content with StaleWhileRevalidate strategy and background sync
+workbox.routing.registerRoute(/\.(html|js)$/, new workbox.strategies.StaleWhileRevalidate({
+  cacheName: CACHE_NAME_DYNAMIC,
+  plugins: [
+    ...cacheSettings.plugins,
+    new workbox.backgroundSync.BackgroundSyncPlugin("bgSyncQueue", {
+      maxRetentionTime: 24 * 60, // Retry for up to 24 hours
+    }),
+  ],
+}));
 
-// Cache dynamic content (HTML, JS) with StaleWhileRevalidate strategy and background sync
-workbox.routing.registerRoute(
-  /\.(html|js)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE_NAME_DYNAMIC,
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [200],
-        headers: { "Cache-Control": "public, max-age=600" }, // Cache for 10 minutes
-      }),
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50, // Adjust as needed
-      }),
-      bgSyncPlugin,
-    ],
-  })
-);
+// Cache navigation requests with StaleWhileRevalidate strategy
+workbox.routing.registerRoute(({ event }) => event.request.mode === "navigate", new workbox.strategies.StaleWhileRevalidate(cacheSettings));
 
-// Cache navigation requests using StaleWhileRevalidate strategy
-workbox.routing.registerRoute(
-  ({ event }) => event.request.mode === "navigate",
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE_NAME_STATIC,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50, // Adjust as needed
-      }),
-    ],
-  })
-);
-
-// Cache everything else using NetworkFirst strategy
-workbox.routing.setDefaultHandler(
-  new workbox.strategies.NetworkFirst({
-    cacheName: CACHE_NAME_STATIC,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50, // Adjust as needed
-        maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-      }),
-    ],
-  })
-);
-
-
+// Cache everything else with NetworkFirst strategy
+workbox.routing.setDefaultHandler(new workbox.strategies.NetworkFirst(cacheSettings));
 
 // Periodic Notifications every 1 minute
 const showPeriodicNotification = async () => {
   const title = "WebCore Backup Reminder";
   const options = {
-    body: "Click here to backup your nostr data",
-    icon: "/assets/icons/icon.png", // Replace with the correct path
+    body: "Don't forget to back up your Nostr data regularly for a seamless experience.",
+    icon: "/assets/icons/icon.png",
+    vibrate: [200, 100, 200], // Vibration pattern
+    badge: "/assets/icons/badge.png", // Displayed in the notification bar
+    data: {
+      openUrl: "/assets/pages/backup/backup.html", // URL to open on notification click
+    },
+    actions: [
+      {
+        action: "backupAction",
+        title: "Backup Now",
+      },
+    ],
   };
 
-  // Use Workbox background sync to ensure reliability
-  await bgSyncPlugin._saveNotification({ title, options });
+  await workbox.backgroundSync.broadcastNotification({ title, options });
 
-  // Display the notification
   self.registration.showNotification(title, options);
 };
 
@@ -145,12 +78,18 @@ const scheduleNotifications = () => {
   setInterval(showPeriodicNotification, 60 * 1000); // Every 1 minute
 };
 
-// Handle notification click to open /list.html
+// Handle notification click to open /list.html or perform backup action
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
 
-  // Open /list.html when notification is clicked
-  const openListPage = clients.openWindow("/assets/pages/backup/backup.html");
+  const openUrl = event.notification.data.openUrl;
+  const backupAction = event.action === "backupAction";
 
-  event.waitUntil(openListPage);
+  if (backupAction) {
+    // Perform backup action if "Backup Now" is clicked
+    // Add your backup logic here
+  } else if (openUrl) {
+    // Open the specified URL on notification click
+    event.waitUntil(clients.openWindow(openUrl));
+  }
 });
