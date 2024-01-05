419db9540ee4a5322574151068d2e2b3beaf04ee
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-05 12:21:47 +0000

    main

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ export default {
   build: {
     rollupOptions: {
       input: {
-        home: "index.html", // your main entry point
-        control: "/assets/pages/control/control.html", // path to your other pages
-        backup: "/assets/pages/backup/backup.html",
+        home: "/index.html", // your main entry point
         wallet: "/assets/pages/wallet/wallet.html",
+        control: "/assets/pages/control/control.html", // path to your other pages
+        backup: "/assets/pages/backup/nostrdb/index.html",
+        wallet: "/assets/pages/wallet/wallet.html",
+        
         // add more entries for additional pages as needed
       },
     },
