7729c144c1ad39f48b58696939485121ce78b769
Iefan <66840141+realiefan@users.noreply.github.com>
2024-01-09 16:25:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-// Import the Workbox library
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-// Set up Workbox with stale-while-revalidate strategy for static assets
 workbox.routing.registerRoute(
   /\.(css|js|png|jpg|jpeg|gif|svg|ico)$/,
   new workbox.strategies.StaleWhileRevalidate({
@@ -9,27 +7,25 @@ workbox.routing.registerRoute(
   })
 );
 
-// Set up caching strategy for images from the specified API
+// Add a new route for the Icon Horse API
 workbox.routing.registerRoute(
-  ({ request }) => request.destination === 'image' && request.url.startsWith('https://icon.horse/icon/'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'NostrNet-icon-images',
+  /https:\/\/icon\.horse\/icon\//,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'IconHorse-api',
   })
 );
 
-// Precache specific assets during installation
 workbox.precaching.precacheAndRoute([
   { url: '/index.html', revision: '123456' },
   // Add other assets you want to precache
 ]);
 
-// Delete all previous caches during activation
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName.startsWith('NostrNet') && cacheName !== 'NostrNet-static-assets') {
+          if (cacheName.startsWith('NostrNet') && cacheName !== 'NostrNet-static-assets' && cacheName !== 'IconHorse-api') {
             return caches.delete(cacheName);
           }
         })
@@ -37,3 +33,4 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
